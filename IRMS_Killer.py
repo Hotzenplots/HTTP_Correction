4e854436439e7add6587f1908bef07452dd34593
Hotzenplots <yangyingzhen@hotmail.com>
2021-01-09 20:12:33 +0800

    P5完成,需要个辅助函数

--- IRMS_Killer.py
+++ IRMS_Killer.py
@@ -14,8 +14,9 @@ File_Name = '豪德置业'
 
 P1_Push_Box                 = False
 P2_Generate_Support_Segment = False
-P3_Generate_Cable_Segment   = True
-P4_Cable_Lay                = True
+P3_Generate_Cable_Segment   = False
+P4_Cable_Lay                = False
+P5_Generate_ODM_and_Tray    = True
 
 def File_Process_and_Generate_Basic_Data():
     '''读取本地数据,在不查询的前提下填充几个List'''
@@ -90,10 +91,8 @@ def File_Process_and_Generate_Basic_Data():
             if List_Box_Info[box_num]['Box_Type'] == List_Box_Type[row_data][0]:
                 List_Box_Info[box_num]['Box_Type_ID'] = List_Box_Type[row_data][1]
                 List_Box_Info[box_num]['ResPoint_Type_ID'] = List_Box_Type[row_data][2]
-        for row_data in range(len(List_Template)):
-            if List_7013[1][1] == List_Template[row_data][2]:
-                List_Box_Info[box_num]['Box_City_ID'] = List_Template[row_data][1]
-                List_Box_Info[box_num]['Box_County_ID'] = List_Template[row_data][3]
+        List_Box_Info[box_num]['City_ID'] = List_Template_Selected[1]
+        List_Box_Info[box_num]['County_ID'] = List_Template_Selected[16]
 
     '''读取并整理Sheet_OCS_List,生成List_CS_Info'''
     WS_obj = WB_obj['OCS_List']
@@ -367,43 +366,145 @@ def Cable_Lay(Para_List_CS_Info):
     List_Cable_Lay_State = Response_Body.xpath('//@msg')
     print('P4-敷设-{}-{}'.format(List_Cable_Lay_State[0], Para_List_CS_Info['Cable_Seg_Name']))
 
+def Generate_Topology():
+    global CS_Topology
+    CS_Topology = []
+    Line_Num = 0
+    Segment_Num = 0
+    for cable_seg_num in range(len(List_CS_Info)):
+        if cable_seg_num == 0:
+            CS_Topology.append([List_CS_Info[cable_seg_num]['A_Box_Name']])
+            CS_Topology[cable_seg_num].append(List_CS_Info[cable_seg_num]['Z_Box_Name'])
+            Segment_Num += 1
+            continue
+        if List_CS_Info[cable_seg_num]['A_Box_Name'] == CS_Topology[Line_Num][Segment_Num]:
+            CS_Topology[Line_Num].append(List_CS_Info[cable_seg_num]['Z_Box_Name'])
+            Segment_Num += 1
+            continue
+        else:
+            CS_Topology.append([List_CS_Info[cable_seg_num]['A_Box_Name']])
+            Line_Num += 1
+            Segment_Num = 1
+            CS_Topology[Line_Num].append(List_CS_Info[cable_seg_num]['Z_Box_Name'])
+
+def Generate_FS_Info():
+    #filled by 0
+    for box_info in List_Box_Info:
+        box_info['1FS_Count'] = 0
+        box_info['2FS_Count'] = 0
+        box_info['DL_2FS_Count'] = 0
+        box_info['ODM_Rows'] = 0
+        box_info['Tray_Count'] = 0
+    #1fs_count&2fs_count
+    for row in List_7013:
+        for box_info in List_Box_Info:
+            if row[3] == box_info['Box_Name']:
+                if row[10] == '一级':
+                    box_info['1FS_Count'] += 1
+                elif row[10] == '二级':
+                    box_info['2FS_Count'] += 1
+    #dl_2fs_count
+    for cable_num in range(len(CS_Topology) - 1, -1, -1):
+        for cable_seg_num in range(len(CS_Topology[cable_num]) - 1, -1, -1):
+            if cable_seg_num == len(CS_Topology[cable_num]) - 1:#Tail
+                for box_info in List_Box_Info: 
+                    if CS_Topology[cable_num][cable_seg_num] == box_info['Box_Name']:
+                        box_info['DL_2FS_Count'] = 0
+            elif cable_seg_num == 0:#Head
+                for box_info in List_Box_Info:
+                    if CS_Topology[cable_num][cable_seg_num] == box_info['Box_Name']:
+                        box_info['DL_2FS_Count'] = 0
+            else:#Middle
+                for box_info in List_Box_Info:
+                    if CS_Topology[cable_num][cable_seg_num + 1] == box_info['Box_Name']:
+                        DL_2FS_Count_temp = box_info['2FS_Count'] + box_info['DL_2FS_Count']
+                for box_info in List_Box_Info:
+                    if CS_Topology[cable_num][cable_seg_num] == box_info['Box_Name']:
+                        box_info['DL_2FS_Count'] = DL_2FS_Count_temp
+    #dl_2fs_count 1fs_box_correction
+    for box_info in List_Box_Info:
+        if box_info['1FS_Count'] != 0:
+            DL_2FS_Count_temp = []
+            for cable_num in CS_Topology:
+                if box_info['Box_Name'] == cable_num[0]:
+                    for  box_info_2 in List_Box_Info:
+                        if cable_num[1] == box_info_2['Box_Name']:
+                            DL_2FS_Count_temp.append(str(box_info_2['DL_2FS_Count'] + box_info_2['2FS_Count']))
+                            box_info['DL_2FS_Count'] = '&'.join(DL_2FS_Count_temp)
+    #ODM_Rows$Tray_Count
+    for box_info in List_Box_Info:
+        if box_info['1FS_Count'] == 0:
+            for cable_seg_info in List_CS_Info:
+                if box_info['Box_Name'] == cable_seg_info['Z_Box_Name']:
+                    box_info['ODM_Rows'] = box_info['Tray_Count'] = math.ceil(cable_seg_info['Width'] / 12)
+        elif box_info['1FS_Count'] != 0:
+            for cable_seg_info in List_CS_Info:
+                if box_info['Box_Name'] == cable_seg_info['A_Box_Name']:
+                    box_info['ODM_Rows'] = box_info['Tray_Count'] = math.ceil(cable_seg_info['Width'] / 12) + box_info['ODM_Rows']
+
+def Generate_ODM(Para_List_Box_Info):
+    URL_Generate_ODM = 'http://10.209.199.74:8120/nxapp/room/editODMData.ilf'
+    Form_Info = '<params><odm id="" rowflag="+" rownum="1" colflag="+" colnum="12"><attribute module_rowno="1" rowno="'+str(Para_List_Box_Info['ODM_Rows'])+'" columnno="12" terminal_arr="0" maintain_county="'+str(Para_List_Box_Info['County_ID'])+'" maintain_city="'+str(Para_List_Box_Info['City_ID'])+'" structure_id="'+str(Para_List_Box_Info['ResPoint_ID'])+'" structure_type="'+str(Para_List_Box_Info['ResPoint_Type_ID'])+'" related_rack="'+str(Para_List_Box_Info['Box_ID'])+'" related_type="'+str(Para_List_Box_Info['Box_Type_ID'])+'" status="8" model="odm"/></odm></params>'
+    Form_Info_Tail = '<params><param key="pro_task_id" value=""/><param key="status" value="8"/><param key="photo" value="null"/><param key="isvirtual" value="0"/><param key="virtualtype" value=""/></params>'
+    Form_Info_Encoded = "params=" + parse.quote_plus(Form_Info) + "&model=odm&" +  "lifeparams=" + parse.quote_plus(Form_Info_Tail)
+    Request_Lenth = chr(len(Form_Info_Encoded))
+    Request_Header = {'Host': '10.209.199.74:8120','Content-Type': 'application/x-www-form-urlencoded','Content-Length': Request_Lenth}
+    Response_Body = requests.post(URL_Generate_ODM, data=Form_Info_Encoded, headers=Request_Header)
+    Response_Body = parse.unquote(bytes(Response_Body.text, encoding="utf-8"))
+    Response_Body = etree.HTML(Response_Body)
+    List_ODM_ID = Response_Body.xpath('//@int_id')
+    Para_List_Box_Info['ODM_ID'] = List_ODM_ID[0]
+    print('P5-ODM-{}-in-{}'.format(List_ODM_ID[0], Para_List_Box_Info['Box_Name']))
+
+def Generate_Tray(Para_List_Box_Info):
+    URL_Generate_Tray = "http://10.209.199.74:8120/nxapp/room/editTray_sx.ilf"
+    Form_Info = '<params model="tray"><obj related_rack="'+str(Para_List_Box_Info['Box_ID'])+'" related_type="'+str(Para_List_Box_Info['Box_Type_ID'])+'" structure_id="'+str(Para_List_Box_Info['ResPoint_ID'])+'" structure_type="'+str(Para_List_Box_Info['ResPoint_Type_ID'])+'" deviceshelf_id="'+str(Para_List_Box_Info['ODM_ID'])+'" tray_no="1" tray_num="'+str(Para_List_Box_Info['Tray_Count'])+'" row_count="1" col_count="12" int_id=""/></params>'
+    Form_Info_Tail = '<params><param key="pro_task_id" value=""/><param key="status" value="8"/><param key="photo" value="null"/><param key="isvirtual" value="0"/><param key="virtualtype" value=""/></params>'
+    Form_Info_Encoded = "params=" + parse.quote_plus(Form_Info) + "&model=odm&" +  "lifeparams=" + parse.quote_plus(Form_Info_Tail)
+    Request_Lenth = chr(len(Form_Info_Encoded))
+    Request_Header = {'Host': '10.209.199.74:8120','Content-Type': 'application/x-www-form-urlencoded','Content-Length': Request_Lenth}
+    Response_Body = requests.post(URL_Generate_Tray, data=Form_Info_Encoded, headers=Request_Header)
+    Response_Body = parse.unquote(bytes(Response_Body.text, encoding="utf-8"))
+    Response_Body = etree.HTML(Response_Body)
+    List_Terminal_IDs = Response_Body.xpath('//terminal//@int_id')
+    print('P5-Terminal_IDs_Count-{}'.format(len(List_Terminal_IDs)))
+    Para_List_Box_Info['Terminal_IDs'] = '&'.join(List_Terminal_IDs)
+
 if __name__ == '__main__':
+
     File_Process_and_Generate_Basic_Data()
+    Swimming_Pool(Query_Box_ID_ResPont_ID_Alias, List_Box_Info)
+    if P2_Generate_Support_Segment or P3_Generate_Cable_Segment or P4_Cable_Lay:
+        Prepare_Support_Sys_and_Cable_Sys()
+        Query_Project_Code_ID()
+    if P5_Generate_ODM_and_Tray:
+        Generate_Topology()
+        Generate_FS_Info()
+
     if P1_Push_Box:
         print('P1-开始')
-        Swimming_Pool(Query_Box_ID_ResPont_ID_Alias, List_Box_Info)
         Swimming_Pool(Push_Box, List_Box_Info)
         print('P1-结束')
     if P2_Generate_Support_Segment:
         print('P2-开始')
-        if not P1_Push_Box:
-            Swimming_Pool(Query_Box_ID_ResPont_ID_Alias, List_Box_Info)
-        Prepare_Support_Sys_and_Cable_Sys()
-        Query_Project_Code_ID()
         Generate_Support_Segment()
         print('P2-结束')
     if P3_Generate_Cable_Segment:
         print('P3-开始')
-        if (not P1_Push_Box) and (not P2_Generate_Support_Segment):
-            Swimming_Pool(Query_Box_ID_ResPont_ID_Alias, List_Box_Info)
-        Prepare_Support_Sys_and_Cable_Sys()
-        Query_Project_Code_ID()
         Generate_Cable_Segment()
         print('P3-结束')
     if P4_Cable_Lay:
         print('P4-开始')
-        if (not P1_Push_Box) and (not P2_Generate_Support_Segment) and (not P3_Generate_Cable_Segment):
-            Swimming_Pool(Query_Box_ID_ResPont_ID_Alias, List_Box_Info)
-            Prepare_Support_Sys_and_Cable_Sys()
-            Query_Project_Code_ID()
         Swimming_Pool(Query_Support_Seg_ID_Cable_Seg_ID, List_CS_Info)
         Swimming_Pool(Cable_Lay, List_CS_Info)
         print('P4-结束')
-        # Dic_Test = {'A_Box_Name': '太原阳曲县山西豪德置业有限公司企业宽带GF2006号西楼道GF3006', 'Z_Box_Name': '太原阳曲县山西豪德置业有限公司企业宽带GF2007号西楼道GF3007', 'Width': 24, 'A_Box_Type_ID': 9204, 'A_Box_Type': 'guangfenxianxiang', 'A_ResPoint_Type_ID': 9115, 'A_Longitude': 112.717937063552, 'A_Latitude': 38.160451496305804, 'Z_Box_Type_ID': 9204, 'Z_Box_Type': 'guangfenxianxiang', 'Z_ResPoint_Type_ID': 9115, 'Z_Longitude': 112.718337063552, 'Z_Latitude': 38.160451496305804, 'Length': 35, 'Business_Level': 8, 'Life_Cycle': 8, 'Owner_Type': 1, 'Owner_Name': 0, 'Field_Type': '市城区域', 'City_ID': 445835190, 'County_ID': 445835318, 'DQS_Project_ID': 246552123, 'DQS_ID': 246552126, 'DQS_County_ID': 483582248, 'DQS_Maintainer_ID': 685585124, 'Task_Name_ID': '2870791671', 'A_Box_ID': '730421234', 'A_ResPoint_ID': '730421314', 'Z_Box_ID': '730421235', 'Z_ResPoint_ID': '730421315', 'Support_Sys_ID': '805438309', 'Cable_Sys_ID': '805427890', 'Project_Code_ID': '54487', 'Support_Seg_Name': '太原阳曲县山西豪德置业有限公司企业宽带GF2006号西楼道GF3006资源点-太原阳曲县山西豪德置业有限公司企业宽带GF2007号西楼道GF3007资源点引上段', 'Support_Seg_ID': '805553969', 'Cable_Seg_Name': '太原阳曲县山西豪德置业有限公司企业宽带GF2006号西楼道GF3006资源点-太原阳曲县山西豪德置业有限公司企业宽带GF2007号西楼道GF3007资源点光缆段', 'Cable_Seg_ID': '807387045'}
-        # Cable_Lay(Dic_Test)
+    if P5_Generate_ODM_and_Tray:
+        print('P5-开始')
+        # Swimming_Pool(Generate_ODM, List_Box_Info)
+        Swimming_Pool(Generate_Tray, List_Box_Info)
+        print('P5-结束')
+
+
         # print(sorted(List_CS_Info[10].items(), key = lambda item:item[0]))
         # print(List_CS_Info[69])
         # print(List_Box_Info[20])
-        # Dic_Test = {'A_Box_Name': '太原阳曲县山西豪德置业有限公司企业宽带7号西楼道GF0096', 'Z_Box_Name': '太原阳曲县山西豪德置业有限公司企业宽带GF1001号西楼道GF1001', 'Width': 24, 'A_Box_Type_ID': 9204, 'A_Box_Type': 'guangfenxianxiang', 'A_ResPoint_Type_ID': 9115, 'A_Longitude': 112.715537063552, 'A_Latitude': 38.1612514963058, 'Z_Box_Type_ID': 9204, 'Z_Box_Type': 'guangfenxianxiang', 'Z_ResPoint_Type_ID': 9115, 'Z_Longitude': 112.715937063552, 'Z_Latitude': 38.1612514963058, 'Length': 35, 'Business_Level': 8, 'Life_Cycle': 8, 'Owner_Type': 1, 'Owner_Name': 0, 'Field_Type': '市城区域', 'City_ID': 445835190, 'County_ID': 445835318, 'DQS_Project_ID': 246552123, 'DQS_ID': 246552126, 'DQS_County_ID': 483582248, 'DQS_Maintainer_ID': 685585124, 'Task_Name_ID': '2870791671', 'A_Box_ID': '730421212', 'A_ResPoint_ID': '730421292', 'Z_Box_ID': '730421213', 'Z_ResPoint_ID': '730421293', 'Support_Sys_ID': '805438309', 'Cable_Sys_ID': '805427890', 'Project_Code_ID': '54487'}
-        # Query_Support_Seg_ID_Cable_Seg_ID(Dic_Test)
-
